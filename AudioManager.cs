0fcad49a62cde2e230e6bc966482d4772d9da2fa
Ashay <112194962+ashaydave@users.noreply.github.com>
2024-01-21 12:47:22 -0500

    Added a couple of error handling debugs

--- AudioManager.cs
+++ AudioManager.cs
@@ -212,7 +212,6 @@ public class AudioManager : MonoBehaviour
                     return null;
                 }
             }
-            return audioClipToPlay.audioMixerGroup;
         }
         return null;
     }
